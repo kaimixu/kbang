c68548d69b38d3b57f979c0749378197d8380ea0
kaimixu <kaimixu@hotmail.com>
2016-09-20 11:23:06 +0800

    Add files via upload

---
+++ robot/print.go
@@ -0,0 +1,70 @@
+package robot
+
+import (
+	"fmt"
+	"time"
+)
+
+type result struct {
+	statusCode    int
+	duration      time.Duration
+}
+
+type output struct {
+	average  float64
+	rps      float64
+
+	n 			int
+	c 			int
+	reqNumTotal 		int64
+	reqNumFail		int64
+	reqNumSucc		int64
+	costTimeTotal 		float64
+	results chan *result
+}
+
+func newOutput(n int, c int) *output {
+	return &output{
+		n: n,
+		c: c,
+		results: make(chan *result, n),
+	}
+}
+
+func (this *output) addResult(res *result) {
+	this.results <- res
+}
+
+func (this *output) finalize() {
+
+	for {
+		select {
+		case res := <-this.results:
+			this.reqNumTotal++
+			if res.statusCode != 200 {
+				this.reqNumFail++
+			}else {
+				this.reqNumSucc++
+			}
+			this.costTimeTotal += res.duration.Seconds()
+		default:
+			this.rps = float64(this.reqNumTotal) / this.costTimeTotal
+			this.average = this.costTimeTotal / float64(this.reqNumTotal)
+			this.print()
+			return
+		}
+	}
+}
+
+func (this *output) print() {
+	if this.reqNumTotal > 0 {
+		fmt.Printf("Summary:\n")
+		fmt.Printf("  Concurrency Level:\t%d\n", this.c)
+		fmt.Printf("  Time taken for tests:\t%4.4f secs\n", this.costTimeTotal)
+		fmt.Printf("  Complete requests:\t%d\n", this.reqNumTotal)
+		fmt.Printf("  Failed requests:\t%d\n", this.reqNumFail)
+		fmt.Printf("  Success requests:\t%d\n", this.reqNumSucc)
+		fmt.Printf("  Requests per second:\t%4.4f\n", this.rps)
+		fmt.Printf("  Average time per request:\t%4.4f\n", this.average)
+	}
+}
